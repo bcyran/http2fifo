a2409f777895cc43019a46e61b14fdcf5f57cbda
Bazyli Cyran <bazyli@cyran.dev>
2026-06-06 21:04:09 +0200

    chore: init repo and toolchain

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    ...
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+
+    forEachSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f (import nixpkgs {
+          inherit system;
+          overlays = [
+            rust-overlay.overlays.default
+          ];
+        }));
+  in {
+    devShells = forEachSystem (pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+          cargo-llvm-cov
+          just
+        ];
+
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
+      };
+    });
+
+    formatter = forEachSystem (pkgs: pkgs.alejandra);
+  };
+}
